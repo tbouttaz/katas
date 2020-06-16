ac836dde1a93799b2edde529951bd18fe8c3851e
Tom Bouttaz <Thomas_Bouttaz@Dellteam.com>
2020-06-16 16:43:51 +0100

    Add other passing cases to test behaviour in 1st case

--- fizz-buzz/src/test/java/org/kata/FizzBuzzTest.java
+++ fizz-buzz/src/test/java/org/kata/FizzBuzzTest.java
@@ -10,5 +10,13 @@ public class FizzBuzzTest {
     @Test
     public void should_return_number_as_string() {
         assertThat(new FizzBuzz().convert(1), is("1"));
+        assertThat(new FizzBuzz().convert(2), is("2"));
+        assertThat(new FizzBuzz().convert(4), is("4"));
+        assertThat(new FizzBuzz().convert(4), is("4"));
+        assertThat(new FizzBuzz().convert(7), is("7"));
+        assertThat(new FizzBuzz().convert(8), is("8"));
+        assertThat(new FizzBuzz().convert(11), is("11"));
+        assertThat(new FizzBuzz().convert(13), is("13"));
+        assertThat(new FizzBuzz().convert(14), is("14"));
     }
 }
