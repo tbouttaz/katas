2862889c3e6832882712f6f3e097847caefc9c20
Tom Bouttaz <Thomas_Bouttaz@Dellteam.com>
2020-06-16 16:49:06 +0100

    Refactor 1st test: initialisation before each test

--- fizz-buzz/src/test/java/org/kata/FizzBuzzTest.java
+++ fizz-buzz/src/test/java/org/kata/FizzBuzzTest.java
@@ -1,5 +1,6 @@
 package org.kata;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -7,16 +8,24 @@ import static org.hamcrest.CoreMatchers.*;
 
 public class FizzBuzzTest {
 
+    private FizzBuzz fizzBuzz;
+
+    @Before
+    public void init(){
+        fizzBuzz = new FizzBuzz();
+    }
+
     @Test
     public void should_return_number_as_string() {
-        assertThat(new FizzBuzz().convert(1), is("1"));
-        assertThat(new FizzBuzz().convert(2), is("2"));
-        assertThat(new FizzBuzz().convert(4), is("4"));
-        assertThat(new FizzBuzz().convert(4), is("4"));
-        assertThat(new FizzBuzz().convert(7), is("7"));
-        assertThat(new FizzBuzz().convert(8), is("8"));
-        assertThat(new FizzBuzz().convert(11), is("11"));
-        assertThat(new FizzBuzz().convert(13), is("13"));
-        assertThat(new FizzBuzz().convert(14), is("14"));
+        assertThat(fizzBuzz.convert(1), is("1"));
+        assertThat(fizzBuzz.convert(2), is("2"));
+        assertThat(fizzBuzz.convert(4), is("4"));
+        assertThat(fizzBuzz.convert(4), is("4"));
+        assertThat(fizzBuzz.convert(7), is("7"));
+        assertThat(fizzBuzz.convert(8), is("8"));
+        assertThat(fizzBuzz.convert(11), is("11"));
+        assertThat(fizzBuzz.convert(13), is("13"));
+        assertThat(fizzBuzz.convert(14), is("14"));
     }
+
 }
