7881e2505ed62666eb7edb25fdbed14da240afc2
Tom Bouttaz <Thomas_Bouttaz@Dellteam.com>
2020-06-16 16:52:07 +0100

    Add most simple implementation to make Test 2 pass

--- fizz-buzz/src/main/java/org/kata/FizzBuzz.java
+++ fizz-buzz/src/main/java/org/kata/FizzBuzz.java
@@ -2,6 +2,10 @@ package org.kata;
 
 public class FizzBuzz {
     public String convert(int number) {
+        if (number == 3) {
+            return "Fizz";
+        }
+
         return String.valueOf(number);
     }
 }
